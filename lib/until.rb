52ef94be2502fc09a45608d4062b45cecc65a51b
Tammy L Leonard-Bruton <enigmalb@outlook.com>
2019-11-11 23:30:34 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,15 @@
+
+
+
+
+
+
 def using_until
+ # counter = 0
   levitation_force = 6
-  #your code here
-    
+  until counter == 10
+    puts "Wingardium Leviosa"
+    counter += 1
+  end
 end
 
--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,14 @@
-def using_while
-  levitation_force = 6
-	
-  #your code here
-end
 
+
+
+
+
+def using_while
+ # counter = 0
+   levitation_force = 6
+  while counter < 10
+    puts "Wingardium Leviosa"   
+   counter += 1
+  end
+end
 
